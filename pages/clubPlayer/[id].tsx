69f1d15c6e98e2ae049cacf45eb53daf7a22a3c4
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-05-17 21:05:25 +0900

    dynamic pages err

--- pages/clubPlayer/[id].tsx
+++ pages/clubPlayer/[id].tsx
@@ -3,8 +3,8 @@
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
 import GlobalContextsProvider from "../../components/plasmic/proliga_clone/PlasmicGlobalContextsProvider";
-import { ScreenVariantProvider } from "../../components/plasmic/proliga_clone/PlasmicGlobalVariant__Screen";
-import { UnnamedGlobalGroupOfVariantsContext } from "../../components/plasmic/proliga_clone/PlasmicGlobalVariant__UnnamedGlobalGroupOfVariants";
+// import { ScreenVariantProvider } from "../../components/plasmic/proliga_clone/PlasmicGlobalVariant__Screen";
+// import { UnnamedGlobalGroupOfVariantsContext } from "../../components/plasmic/proliga_clone/PlasmicGlobalVariant__UnnamedGlobalGroupOfVariants";
 import { PlasmicMoreInfo } from "../../components/plasmic/proliga_clone/PlasmicMoreInfo";
 import { useRouter } from "next/router";
 
